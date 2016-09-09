af7d6c10a3661e1d33762b4ef0e4be36996bb8a4
Esmery Corniel <esmerycornielle@gmail.com>
2016-09-09 17:57:44 -0400

    Done

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  PIRATES = []
+  def initialize(name, type, booty)
+    @name, @type, @booty = name, type, booty
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
+  def self.clear
+    PIRATES.clear
+  end
+
 end
